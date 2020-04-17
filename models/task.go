d43fa02d2e14a39c36447d4bb45468722b99afde
Boy Sandy Gladies Arriezona <reno.esper@gmail.com>
2020-04-17 14:37:46 +0700

    refactor(models): Add new fields

--- models/task.go
+++ models/task.go
@@ -12,5 +12,7 @@ type (
 		Content   string    `json:"content"`
 		Tags      string    `json:"tags"`
 		CreatedAt time.Time `json:"created_at"`
+		IsDeleted bool      `json:"is_deleted"`
+		IsDone    bool      `json:"is_done"`
 	}
 )
