268c26d61f20e2bc7eb5aaae603b2345551f6f9c
Boy Sandy Gladies Arriezona <reno.esper@gmail.com>
2020-04-17 14:42:19 +0700

    feat(services): Enable connection checking for determining the uses of repository

--- services/task/task.go
+++ services/task/task.go
@@ -3,11 +3,22 @@ package task
 import (
 	"gitlab.com/renodesper/efishery-cli-app/models"
 	"gitlab.com/renodesper/efishery-cli-app/repositories/couchdb"
+	"gitlab.com/renodesper/efishery-cli-app/repositories/sqlite3"
 )
 
 type (
+	// DBRepository ...
+	DBRepository interface {
+		GetTask(docID string) (*models.Task, error)
+		GetTasks() ([]*models.Task, error)
+		AddTask(task *models.Task) error
+		UpdateTask(task *models.Task) error
+		DeleteTask(task *models.Task) error
+		DoneTask(task *models.Task) error
+	}
+
 	task struct {
-		repository couchdb.CouchDBRepository
+		repository DBRepository
 	}
 
 	// TaskService ...
@@ -15,13 +26,22 @@ type (
 		GetTasks() ([]*models.Task, error)
 		AddTask(task *models.Task) error
 		UpdateTask(task *models.Task) error
-		DeleteTask(rev string) error
+		DeleteTask(docID string) error
+		DoneTask(docID string) error
 	}
 )
 
 // NewTaskService ...
 func NewTaskService() TaskService {
+	sqlite := sqlite3.NewSqlite3Repository()
+
+	if couchdb.HasConnection() {
+		return &task{
+			repository: couchdb.NewCouchDBRepository(sqlite),
+		}
+	}
+
 	return &task{
-		repository: couchdb.NewCouchDBRepository(),
+		repository: sqlite,
 	}
 }
