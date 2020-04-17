d7f6d387c011091fd49007b68420a81089611fdb
Boy Sandy Gladies Arriezona <reno.esper@gmail.com>
2020-04-17 14:38:43 +0700

    feat(repositories): Add Sqlite3 repository interface

---
+++ repositories/sqlite3/sqlite3.go
@@ -0,0 +1,42 @@
+package sqlite3
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/spf13/viper"
+	"gitlab.com/renodesper/efishery-cli-app/models"
+)
+
+type (
+	sqlite3 struct {
+		db *sql.DB
+	}
+
+	// Sqlite3Repository ...
+	Sqlite3Repository interface {
+		GetTask(docID string) (*models.Task, error)
+		GetTasks() ([]*models.Task, error)
+		AddTask(task *models.Task) error
+		UpdateTask(task *models.Task) error
+		DeleteTask(task *models.Task) error
+		DoneTask(task *models.Task) error
+	}
+)
+
+// NewSqlite3Repository ...
+func NewSqlite3Repository() Sqlite3Repository {
+	dbname := viper.GetString("app.dbname")
+
+	db, err := sql.Open("sqlite3", fmt.Sprintf("%s.db", dbname))
+	if err != nil {
+		panic(err)
+	}
+
+	db.Exec(fmt.Sprintf("CREATE TABLE IF NOT EXISTS %s (_id TEXT, _rev TEXT, content TEXT, tags TEXT, created_at DATETIME DEFAULT CURRENT_TIMESTAMP, is_deleted INT DEFAULT 0, is_done INT DEFAULT 0)", dbname))
+
+	return &sqlite3{
+		db: db,
+	}
+}
